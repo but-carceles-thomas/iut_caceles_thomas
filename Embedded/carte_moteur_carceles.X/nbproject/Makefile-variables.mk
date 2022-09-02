236fa7cc46d0de869ee0a706d5919e6ac569d86d
TABLE2 <78029790+Kergourlay-T@users.noreply.github.com>
2022-09-02 11:04:18 +0200

    robot1

---
+++ Embedded/carte_moteur_carceles.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=carte_moteur_carceles.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/carte_moteur_carceles.X.production.hex
