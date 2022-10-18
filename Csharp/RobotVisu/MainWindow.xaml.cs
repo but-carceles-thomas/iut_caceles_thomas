d247ab10255d6bcf2e9c7c7c2ec4ea2348a2b774
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-10-18 12:37:05 +0200

    visu 2

--- Csharp/RobotVisu/MainWindow.xaml.cs
+++ Csharp/RobotVisu/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,48 @@ namespace RobotVisu
     /// </summary>
     public partial class MainWindow : Window
     {
+        ReliableSerialPort serialPort1;
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM9", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.Open();
+        }
+
+        bool toggle = false;
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            if (toggle)
+            {
+                SendMessage();
+                //if (textBoxEmission.Text != "")
+                //    textBoxReception.Text += "\nReçu : " + textBoxEmission.Text;
+                //textBoxEmission.Text = "";
+                //buttonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+            //else
+            //{
+            //    buttonEnvoyer.Background = Brushes.Beige;
+            //}
+
+            //toggle = !toggle;
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+
+           
+        }
+        void SendMessage()
+        {
+            serialPort1.WriteLine(textBoxEmission.Text);
+            ///if (textBoxEmission.Text != "")
+            ///   textBoxReception.Text += "Reçu : " + textBoxEmission.Text;
+            textBoxEmission.Text = "";
         }
     }
 }
