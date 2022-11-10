8a83ab3efdd71757c15bfed028c0b13edb575d70
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-11-10 12:41:40 +0100

    visu3-hexa

--- Csharp/RobotVisu/MainWindow.xaml.cs
+++ Csharp/RobotVisu/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RobotVisu
 {
@@ -23,30 +24,43 @@ namespace RobotVisu
     public partial class MainWindow : Window
     {
         ReliableSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
+        Robot robot = new Robot();
+
         public MainWindow()
         {
             InitializeComponent();
-            serialPort1 = new ReliableSerialPort("COM9", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived; ;
             serialPort1.Open();
+
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+        }
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+            if (robot.receivedText != "")
+            {
+                textBoxReception.Text += robot.receivedText;
+                robot.receivedText = "";
+            }
         }
 
-        bool toggle = false;
+        private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            robot.receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+        bool toggle = true;
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
         {
             if (toggle)
             {
                 SendMessage();
-                //if (textBoxEmission.Text != "")
-                //    textBoxReception.Text += "\nReçu : " + textBoxEmission.Text;
-                //textBoxEmission.Text = "";
-                //buttonEnvoyer.Background = Brushes.RoyalBlue;
+               
             }
-            //else
-            //{
-            //    buttonEnvoyer.Background = Brushes.Beige;
-            //}
-
-            //toggle = !toggle;
         }
 
         private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
@@ -64,6 +78,24 @@ namespace RobotVisu
             ///if (textBoxEmission.Text != "")
             ///   textBoxReception.Text += "Reçu : " + textBoxEmission.Text;
             textBoxEmission.Text = "";
+        }
+        
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            if (toggle)
+            {
+                textBoxReception.Text = "";
+            }
+        }
+
+        byte[] byteList = new byte[20];
+        private void buttontest_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                byteList[i] = (byte)(2 * i);
+            }
+            serialPort1.Write(byteList, 0, 20);
         }
     }
 }
